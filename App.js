3896c453bd07f0459e0fdb88bb96d5699900880b
Gil Lohner <gil@lohner.net>
2024-04-04 00:21:48 +0200

    init

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import Nostr from './components/nostr'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: '#fff' }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Nostr placeholderImageSource="text"/>
     </View>
   );
 }
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/nostr.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { StyleSheet, Image, TextInput, SafeAreaView, Button } from 'react-native';
+// Import the package, NIP-07 signer and NDK event
+import NDK, { NDKEvent, NDKNip07Signer } from "@nostr-dev-kit/ndk";
+
+const nip07signer = new NDKNip07Signer();
+const ndk = new NDK({
+    signer: nip07signer,
+    explicitRelayUrls: ["wss://relay.damus.io", "wss://relay.primal.net", "wss://nos.lol"],
+});
+
+async function helloWorld(message) {
+    console.log("message")
+    console.log(message)
+    await ndk.connect();
+    nip07signer.user().then(async (user) => {
+        if (!!user.npub) {
+            console.log("Permission granted to read their public key:", user.npub);
+        }
+    });
+    
+    const ndkEvent = new NDKEvent(ndk);
+    ndkEvent.kind = 1;
+    ndkEvent.content = message;
+    ndkEvent.publish(); // This will trigger the extension to ask the user to confirm signing.    
+}
+
+export default function Nostr() {
+    const [inputText, onChangeInputText] = React.useState('');    
+    return (
+        <SafeAreaView>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeInputText}
+                value={inputText}
+                placeholder="useless placeholder"
+                keyboardType="numeric"
+            />
+            <Button
+                onPress={() => helloWorld(inputText)}
+                title="Learn More"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        color: "#fff",
+        borderColor: "#f32443",
+      },
+});
